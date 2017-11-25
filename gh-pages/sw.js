c8d919513c282d213074838cd9f87afd84f1c60e
Peter Rybar <pr.rybar@gmail.com>
2017-11-25 18:03:30 +0100

    gh pages mdc

--- gh-pages/sw.js
+++ gh-pages/sw.js
@@ -1,2 +1,27 @@
-!function(e,o){"object"==typeof exports&&"undefined"!=typeof module?o():"function"==typeof define&&define.amd?define(o):o()}(0,function(){"use strict";importScripts("lib/workbox-sw.js"),(new WorkboxSW).precache([])});
+!function(e,o){"object"==typeof exports&&"undefined"!=typeof module?o():"function"==typeof define&&define.amd?define(o):o()}(0,function(){"use strict";importScripts("lib/workbox-sw.js"),(new WorkboxSW).precache([
+  {
+    "url": "index.js",
+    "revision": "4ea229e9e676642ad83af8c15bec0249"
+  },
+  {
+    "url": "lib/incremental-dom-min.js",
+    "revision": "d7839c2cb4ac91773e025ee3a67cecc0"
+  },
+  {
+    "url": "lib/material-components-web.min.css",
+    "revision": "dc2636ed3ab600cc5059c06ef2509d13"
+  },
+  {
+    "url": "lib/material-components-web.min.js",
+    "revision": "a79c4c5a83f1a474a34d625cdd51df2a"
+  },
+  {
+    "url": "lib/workbox-sw.js",
+    "revision": "2a5638f9e33d09efc487b96804a0aa11"
+  },
+  {
+    "url": "sw.js",
+    "revision": "96b24ca4b935104c22fc8c60ad5f5f12"
+  }
+])});
 //# sourceMappingURL=sw.js.map
