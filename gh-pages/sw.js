35327e963b455b3a7ec9694c48eb0a1cce124535
Peter Rybar <pr.rybar@gmail.com>
2017-11-25 17:33:55 +0100

    gh-pages

--- gh-pages/sw.js
+++ gh-pages/sw.js
@@ -1,19 +1,2 @@
-!function(e,o){"object"==typeof exports&&"undefined"!=typeof module?o():"function"==typeof define&&define.amd?define(o):o()}(0,function(){"use strict";importScripts("lib/workbox-sw.js"),(new WorkboxSW).precache([
-  {
-    "url": "index.js",
-    "revision": "3d84e56fa669ba9cf63b76c295ee7eba"
-  },
-  {
-    "url": "lib/incremental-dom-min.js",
-    "revision": "d7839c2cb4ac91773e025ee3a67cecc0"
-  },
-  {
-    "url": "lib/workbox-sw.js",
-    "revision": "2a5638f9e33d09efc487b96804a0aa11"
-  },
-  {
-    "url": "sw.js",
-    "revision": "96b24ca4b935104c22fc8c60ad5f5f12"
-  }
-])});
+!function(e,o){"object"==typeof exports&&"undefined"!=typeof module?o():"function"==typeof define&&define.amd?define(o):o()}(0,function(){"use strict";importScripts("lib/workbox-sw.js"),(new WorkboxSW).precache([])});
 //# sourceMappingURL=sw.js.map
