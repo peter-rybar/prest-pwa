e7fb5564294051f9bbfbd63faa42ee19d10820d1
Peter Rybar <pr.rybar@gmail.com>
2017-12-03 15:48:09 +0100

    map - gh pages

--- gh-pages/sw.js
+++ gh-pages/sw.js
@@ -1,11 +1,39 @@
 !function(e,o){"object"==typeof exports&&"undefined"!=typeof module?o():"function"==typeof define&&define.amd?define(o):o()}(0,function(){"use strict";importScripts("lib/workbox-sw.js"),(new WorkboxSW).precache([
   {
     "url": "index.js",
-    "revision": "3d84e56fa669ba9cf63b76c295ee7eba"
+    "revision": "c71274afc940ef8f18d220b5cfb29c0e"
   },
   {
     "url": "lib/incremental-dom-min.js",
     "revision": "d7839c2cb4ac91773e025ee3a67cecc0"
+  },
+  {
+    "url": "lib/leaflet/images/layers-2x.png",
+    "revision": "4f0283c6ce28e888000e978e537a6a56"
+  },
+  {
+    "url": "lib/leaflet/images/layers.png",
+    "revision": "a6137456ed160d7606981aa57c559898"
+  },
+  {
+    "url": "lib/leaflet/images/marker-icon-2x.png",
+    "revision": "d95d69fa8a7dfe391399e22c0c45e203"
+  },
+  {
+    "url": "lib/leaflet/images/marker-icon.png",
+    "revision": "2273e3d8ad9264b7daa5bdbf8e6b47f8"
+  },
+  {
+    "url": "lib/leaflet/images/marker-shadow.png",
+    "revision": "44a526eed258222515aa21eaffd14a96"
+  },
+  {
+    "url": "lib/leaflet/leaflet.css",
+    "revision": "024b5d7b1c678bac2660aaf7e853a6f2"
+  },
+  {
+    "url": "lib/leaflet/leaflet.js",
+    "revision": "3f4cb4211a380a19b38f985890030f0c"
   },
   {
     "url": "lib/workbox-sw.js",
