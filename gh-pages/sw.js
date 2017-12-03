b122b8a86094495aecd9a70a74a1f672939a9400
Peter Rybar <pr.rybar@gmail.com>
2017-12-04 00:19:58 +0100

    map - gh pages

--- gh-pages/sw.js
+++ gh-pages/sw.js
@@ -1,2 +1,63 @@
-!function(e,o){"object"==typeof exports&&"undefined"!=typeof module?o():"function"==typeof define&&define.amd?define(o):o()}(0,function(){"use strict";importScripts("lib/workbox-sw.js"),(new WorkboxSW).precache([])});
+!function(e,o){"object"==typeof exports&&"undefined"!=typeof module?o():"function"==typeof define&&define.amd?define(o):o()}(0,function(){"use strict";importScripts("lib/workbox-sw.js"),(new WorkboxSW).precache([
+  {
+    "url": "index.js",
+    "revision": "c239cb98a30fc0b11f039d047f317c96"
+  },
+  {
+    "url": "lib/incremental-dom-min.js",
+    "revision": "d7839c2cb4ac91773e025ee3a67cecc0"
+  },
+  {
+    "url": "lib/leaflet-control-geocoder/Control.Geocoder.css",
+    "revision": "303ced394aef5d01c6552e5562f76751"
+  },
+  {
+    "url": "lib/leaflet-control-geocoder/Control.Geocoder.js",
+    "revision": "b27b0a6ce5b2adead34ed9baf2b185cc"
+  },
+  {
+    "url": "lib/leaflet-control-geocoder/images/geocoder.png",
+    "revision": "a987f06fc5d9aaa4e9dfa3df0b37ee22"
+  },
+  {
+    "url": "lib/leaflet/images/layers-2x.png",
+    "revision": "4f0283c6ce28e888000e978e537a6a56"
+  },
+  {
+    "url": "lib/leaflet/images/layers.png",
+    "revision": "a6137456ed160d7606981aa57c559898"
+  },
+  {
+    "url": "lib/leaflet/images/marker-icon-2x.png",
+    "revision": "d95d69fa8a7dfe391399e22c0c45e203"
+  },
+  {
+    "url": "lib/leaflet/images/marker-icon.png",
+    "revision": "2273e3d8ad9264b7daa5bdbf8e6b47f8"
+  },
+  {
+    "url": "lib/leaflet/images/marker-shadow.png",
+    "revision": "44a526eed258222515aa21eaffd14a96"
+  },
+  {
+    "url": "lib/leaflet/leaflet.css",
+    "revision": "024b5d7b1c678bac2660aaf7e853a6f2"
+  },
+  {
+    "url": "lib/leaflet/leaflet.js",
+    "revision": "3f4cb4211a380a19b38f985890030f0c"
+  },
+  {
+    "url": "lib/store.js",
+    "revision": "539d05a3cd7c6180d7d78548e80165eb"
+  },
+  {
+    "url": "lib/workbox-sw.js",
+    "revision": "2a5638f9e33d09efc487b96804a0aa11"
+  },
+  {
+    "url": "sw.js",
+    "revision": "96b24ca4b935104c22fc8c60ad5f5f12"
+  }
+])});
 //# sourceMappingURL=sw.js.map
